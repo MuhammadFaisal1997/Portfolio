a861bc6d476f0d2e5807ee7ae01e1ddfd287e1ec
Muhammad Faisal <muhammadfaisalnafees37@gmail.com>
2024-12-17 03:45:34 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing", {
+    strings: [ "Web Developer", "Freelancer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: [ "Web Developer", "Freelancer", "Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+}); 
